04714e32492ce0cfb4f55375a1040bd31ad244fe
sreenu-reddy <sreenu.nalamalpu@gmail.com>
2022-11-14 03:15:55 +0100

    added navgiation and react router

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import { CategoryMenu } from "./components/category-menu/category-menu.component";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./Routes/home/home.component";
+import Navigation from "./Routes/navigation/navigation.component";
+
+const Shop = () => {
+  return <h2>I am the shop component.</h2>;
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <CategoryMenu categories={categories}></CategoryMenu>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation></Navigation>}>
+        <Route index element={<Home />}></Route>
+        <Route path="shop" element={<Shop></Shop>}></Route>
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
